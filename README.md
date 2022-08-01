5e31547bd41f53ab5c136b6319894d18b1d2a420
C1nth14 <cynthiasusan31@gmail.com>
2022-08-01 20:51:56 +0100

    The ReadMe file is edited and the clean dataset is added to

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# data_cleaning
+# Data Cleaning
+
+For this project, I looked at the dataset al_results_2020 and was tasked with preparing it for analysis. 
+
+### Process
+1. Removing unwanted columns like Zscore, gender and syllabus.
+2. Create a birthday column by combining the birth_day, birth_month and birth_year columns and dropping them.
+3. Removing the rows with missing values that were indicated as '-'.
+4. Removing rows where the student was absent for all the assessments.
+5. Checking that all the dates were valid.
+
+I used functional programming to tackle this project.
+
+Author: Susan Cynthia Musiime
--- al_data.py
+++ al_data.py
@@ -1,21 +1,71 @@
 import pandas as pd
+import csv
 import numpy as np
 
 
-def read_csv(file="al_results_2020.csv"):
+def read_csv(file):
     al_df = pd.read_csv(file, engine='python')
-    print(al_df.columns)
+    return al_df
 
 
-read_csv()
-
-
-def drop_columns(file="al_results_2020.csv"):
-    al_df = pd.read_csv(file, engine='python')
+def drop_columns(al_df):
     to_drop = ['Zscore', 'gender', 'syllabus']
-    new_al = al_df.copy()
-    new_al.drop(to_drop, axis=1, inplace=True)
-    print(new_al.columns)
+
+    return al_df.drop(to_drop, axis=1)
 
 
-drop_columns()
+'''def valid_date(al_df):
+    if al_df[np.logical_and(al_df.birth_month == "February", al_df.birth_year.astype(float) % 400 == 0)]:
+        al_df = al_df.drop(al_df[(al_df['birth_day'] > 29)])
+
+    else:
+        pass
+    return al_df'''
+
+
+def birth_date(al_df):
+    al_df = al_df.assign(birthday=al_df.birth_day.astype(str) + '/'+al_df.birth_month.astype(str)
+                         + '/' + al_df.birth_year.astype(str))
+    to_drop = ['birth_day', 'birth_month', 'birth_year']
+    al_df = al_df.drop(to_drop, axis=1)
+
+    return al_df
+
+
+def drop_rows(al_df):
+    al_df = al_df[al_df.index != '-']
+    al_df = al_df[al_df.stream != '-']
+    al_df = al_df[al_df.sub1 != '-']
+    al_df = al_df[al_df.sub2 != '-']
+    al_df = al_df[al_df.sub3 != '-']
+    al_df = al_df[al_df.island_rank != '-']
+    al_df = al_df[al_df.district_rank != '-']
+
+    return al_df
+
+
+def del_absent(al_df):
+
+    al_df = al_df[al_df.sub1_r != 'Absent']
+    al_df = al_df[al_df.sub2_r != 'Absent']
+    al_df = al_df[al_df.sub3_r != 'Absent']
+    al_df = al_df[al_df.cgt_r != 'Absent']
+    al_df = al_df[al_df.general_english_r != 'Absent']
+
+    return al_df
+
+
+while True:
+    data = read_csv("al_results_2020.csv")
+    data = drop_columns(data)
+    #data = valid_date(data)
+    data = birth_date(data)
+    data = drop_rows(data)
+    data = del_absent(data)
+
+
+
+    data.to_csv('clean_al_df.csv')
+    break
+
+
